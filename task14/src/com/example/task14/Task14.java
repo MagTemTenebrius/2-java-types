404ab710e77dfa9dfe9991687b69998f4dc2bd28
MagTemTenebrius <magtemtenebrius@gmail.com>
2021-09-22 23:26:39 +0500

    task14

--- task14/src/com/example/task14/Task14.java
+++ task14/src/com/example/task14/Task14.java
@@ -4,10 +4,12 @@ public class Task14 {
 
 
     public static int reverse(int value) {
-
-        // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
-
-        return 0;
+        int re_value = 0;
+        while (value > 0) {
+            re_value = 10 * re_value + value % 10;
+            value /= 10;
+        }
+        return re_value;
     }
 
     public static void main(String[] args) {
